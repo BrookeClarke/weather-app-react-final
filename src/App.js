d2eec73a652bb37846da25e2a0c2224f5817c8c8
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-01-11 18:08:14 +0000

    Added a footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <footer>This project was coded by Brooke Clarke and is <a href='https://github.com/BrookeClarke/weather-app-react-final'> open-sourced on GitHub.</a></footer>
       </div>
     </div>
   )
