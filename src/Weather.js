0a2fe51391e4d1701b2c5cc130f09d08d84011db
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-05 19:31:38 +0000

    Started using the SheCodes Weather API

--- src/Weather.js
+++ src/Weather.js
@@ -1,2 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
+
+export default function showWeather(response) {
+    
+}
+
+let apiKey = "0f605ca33b8d413fa995ab3t060267od";
+let apiUrl = 'https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}&units=metric';
+
+axios.get(apiUrl).then(showWeather);
