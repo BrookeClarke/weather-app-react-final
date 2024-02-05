1b2772dd21ff40c36107c250ee93882a45e88c60
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-05 19:33:39 +0000

    Changed the function name

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function showWeather(response) {
+export default function getForecast(response) {
     
 }
 
 let apiKey = "0f605ca33b8d413fa995ab3t060267od";
 let apiUrl = 'https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}&units=metric';
 
-axios.get(apiUrl).then(showWeather);
+axios.get(apiUrl).then(getForecast);
