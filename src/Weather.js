4efb1c99d6fbdee63b44ff2d7d30c420c616bc61
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-07 20:24:34 +0000

    ...

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather() {
+export default function Weather(props) {
     function showForecast(response) {
         console.log(response.data);
     }
-
+    console.log(props);
     let apiKey = "0f605ca33b8d413fa995ab3t060267od";
     let apiUrl = 'https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}';
 
