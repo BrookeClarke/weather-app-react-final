fb97e5064c67b18607d8c792c69ef7e846042700
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-07 20:07:50 +0000

    Fixed errors on calendar.js

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,13 +1,14 @@
 import React, { useState }  from "react";
 
 export default function Calendar(props) {
+    let now = new Date();
     let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    let day = [props.getDay()];
-    let hours = props.date.getHours();
+    let day = days[now.getDay()];
+    let hours = now.getHours();
     if (hours < 10) {
-        hours = `0${hours}`
+        hours = `0${hours}`;
     }
-    let minutes = props.date.getMinutes();
+    let minutes = now.getMinutes();
     if (minutes < 10) {
         minutes = `0${minutes}`
     }
