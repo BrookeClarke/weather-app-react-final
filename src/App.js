12883d9805a10cf6b682ef13a49b2c519f00eb58
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-07 20:01:20 +0000

    Put calendar into App.js, fixed errors on Weather.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from "react";
 import Weather from "./Weather";
+import Calendar from './Calendar';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <Calendar />
         <Weather city="Sydney"/>
         <footer>This project was coded by Brooke Clarke and is <a href='https://github.com/BrookeClarke/weather-app-react-final'> open-sourced on GitHub.</a></footer>
       </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather(props) {
+export default function Weather() {
     let [temperature, setTemperature] = useState(null);
     function showForecast(response) {
         setTemperature(response.data.main.temp);
     }
 
     let apiKey = "0f605ca33b8d413fa995ab3t060267od";
-    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query={props.city}&key=${apiKey}&units=metric';
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query={props.city}&key=${apiKey}';
 
     axios.get(apiUrl).then(showForecast);
 
