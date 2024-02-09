dc32198993f69dbaef241dc5f82cd0ad1de74033
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-09 19:49:53 +0000

    Added a search bar!

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,14 @@
 import React from "react";
+import "./App.css";
 
 export default function Forecast() {
-    
+    return (
+        <div className="html">
+            <h1>Todays Weather</h1>
+            <form className="searchbutton" id="search-form">
+                <input className="city-search" placeholder="Enter a place..." autocomplete="off" autofocus="on" />
+                <input className="search-submit" type="submit" value="search" />
+            </form>
+        </div>
+    )
 }
