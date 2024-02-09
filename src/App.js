3f9a446d96ec5183fc92076a25c1bd0ae91afb95
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-09 19:42:48 +0000

    Added a new file (Forecast.js)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from "react";
-import Weather from "./Weather";
 import Calendar from './Calendar';
+import Forecast from './Forecast';
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <div className="container">
         <h1>Weather App</h1>
         <Calendar />
-        <Weather />
+        <Forecast />
         <footer>This project was coded by Brooke Clarke and is <a href='https://github.com/BrookeClarke/weather-app-react-final'> open-sourced on GitHub.</a></footer>
       </div>
     </div>
