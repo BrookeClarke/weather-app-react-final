53bc3e16f7c0e0f90d5db8dd8376265b10d971d4
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-09 20:38:29 +0000

    CSS

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./App.css";
 
+
 export default function Forecast() {
     return (
         <div className="html">
             <h1>Todays Weather</h1>
-            <form className="searchbutton" id="search-form">
-                <input className="city-search" placeholder="Enter a place..." autocomplete="off" autofocus="on" />
-                <input className="search-submit" type="submit" value="search" />
-                <h2 className="date">Date</h2>
-                <h3>Search for a place</h3>
-            </form>
-            <div className="container">
-
+            <div className="search">
+                <form className="searchbutton" id="search-form">
+                    <input className="city-search" placeholder="Enter a place..." autocomplete="off" autofocus="on" />
+                    <input className="search-submit" type="submit" value="search" />
+                    <h2 className="date">Date</h2>
+                    <h3>Search for a place</h3>
+                </form>
             </div>
+            <div className="container"></div>
         </div>
     )
 }
