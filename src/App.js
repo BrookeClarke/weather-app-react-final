465c11057366d808a6cdff11db8c93b3eca8e942
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-09 20:05:49 +0000

    Added some CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from "react";
-import Calendar from './Calendar';
 import Forecast from './Forecast';
 
 
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-        <Calendar />
         <Forecast />
         <footer>This project was coded by Brooke Clarke and is <a href='https://github.com/BrookeClarke/weather-app-react-final'> open-sourced on GitHub.</a></footer>
       </div>
