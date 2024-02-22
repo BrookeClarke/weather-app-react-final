cb247fe1669dfdc9eb219935ff5be225ffad6c32
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-22 21:45:58 +0000

    HTML

--- src/Forecast.js
+++ src/Forecast.js
@@ -10,8 +10,8 @@ export default function Forecast() {
                 <form className="searchbutton" id="search-form">
                     <input className="city-search" placeholder="Enter a place..." autocomplete="off" autofocus="on" />
                     <input className="search-submit" type="submit" value="search" />
-                    <h2 className="date">Date</h2>
-                    <h3>Search for a place</h3>
+                    <h2 className="date">Thursday 22nd February 2024 21:45pm</h2>
+                    <ul>London</ul>
                 </form>
             </div>
             <div className="container"></div>
