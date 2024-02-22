f8485df67bd70d7aa3dd78e521978102e12d2e15
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-22 21:32:06 +0000

    API Response

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default function Weather() {
     const apiKey = "0f605ca33b8d413fa995ab3t060267od";
     let city = "London";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     const [temperature, setTemperature] = useState(null);
 
     function handleResponse(response) {
@@ -12,9 +12,9 @@ export default function Weather() {
         setTemperature(response.data.main.temp)
     }
 
-
-
     axios.get(apiUrl).then(handleResponse);
+
+
     return (
         <div className="html">
             <h1>Todays Weather</h1>
