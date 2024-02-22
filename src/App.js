ce11b2fc3ca788c990d5100ecc99fda7decc7588
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-22 21:25:48 +0000

    Changed CSS

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
+        <div className="forecast-information">
         <Forecast />
         <footer>This project was coded by Brooke Clarke and is <a href='https://github.com/BrookeClarke/weather-app-react-final'> open-sourced on GitHub.</a></footer>
+      </div>
       </div>
     </div>
   )
