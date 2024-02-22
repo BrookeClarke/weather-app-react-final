bec9a2af6271ba8d27115e150e2ad02a2cf477be
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-22 21:30:00 +0000

    API work

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Forecast from './Forecast';
 function App() {
   return (
     <div className="App">
-      <div className="container">
+      <div className="weather">
         <h1>Weather App</h1>
-        <div className="forecast-information">
+        <div className="container">
         <Forecast />
         <footer>This project was coded by Brooke Clarke and is <a href='https://github.com/BrookeClarke/weather-app-react-final'> open-sourced on GitHub.</a></footer>
       </div>
--- src/Weather.js
+++ src/Weather.js
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function Weather() {
+    const apiKey = "0f605ca33b8d413fa995ab3t060267od";
+    let city = "London";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     const [temperature, setTemperature] = useState(null);
+
     function handleResponse(response) {
         console.log(response.data);
         setTemperature(response.data.main.temp)
     }
-    let city = "London";
-    let apiKey = "0f605ca33b8d413fa995ab3t060267od";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+
 
     axios.get(apiUrl).then(handleResponse);
     return (
