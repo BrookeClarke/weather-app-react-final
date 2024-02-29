c5436df0104d4a5b524907979ea8413c621b2985
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-29 20:53:04 +0000

    Removed Temperature const

--- src/Weather.js
+++ src/Weather.js
@@ -5,12 +5,10 @@ export default function Weather(props) {
     const apiKey = "0f605ca33b8d413fa995ab3t060267od";
     let city = "London";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    const [temperature, setTemperature] = useState(null);
     console.log(props);
 
     function handleResponse(response) {
-        console.log(response.data);
-        setTemperature(response.data.main.temp)
+
     }
 
     axios.get(apiUrl).then(handleResponse);
