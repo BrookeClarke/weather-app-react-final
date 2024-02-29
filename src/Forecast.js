7909b3bf3c684093d5f013ba58f2f8a36c03d925
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-29 21:13:24 +0000

    Added states for Weather

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 
 export default function Forecast() {
--- src/Weather.js
+++ src/Weather.js
@@ -5,11 +5,52 @@ export default function Weather(props) {
     const apiKey = "0f605ca33b8d413fa995ab3t060267od";
     let city = "London";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    console.log(props);
+    const [ready, setReady] = useState(false);
+    const [weatherData, setWeatherData] = useState({});
 
     function handleResponse(response) {
-
+        setWeatherData({
+            city: response.data.name,
+            temperature: response.data.main.temp,
+            description: "Mostly Cloudy",
+            precipitation: 20,
+            humidity: 73,
+            wind: 14,
+        })
     }
 
+    if (ready) {
+        return (
+            <div className="html">
+            <div className="container">
+                <h1>Todays Weather</h1>
+                <div className="search">
+                    <form className="searchbutton" id="search-form">
+                        <input className="city-search" type="search" placeholder="Enter a place..." autocomplete="off" autofocus="on" />
+                        <input className="search-submit" type="submit" value="search" />
+                    </form>
+                </div>
+                    <h2 className="date">Thursday 22nd February 2024 21:45pm</h2>
+                    <ul>
+                        <li>{weatherData.city}</li>
+                        <li>{weatherData.description}</li>
+                    </ul>
+                <div className="row">
+                    <div className="col-6">
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" alt="Mostly Cloudy Weather Icon" />
+                        <h4>6°C</h4>
+                    </div>
+                    <div className="col-6">
+                        <ul>
+                            <li>{weatherData.precipitation}%</li>
+                            <li>{weatherData.humidity}%</li>
+                                    <li>{weatherData.wind}km/h</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+    }
     axios.get(apiUrl).then(handleResponse);
 }
