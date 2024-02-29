9d64403b1977e175cfcc197b6e58e5ea5d4844c6
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-02-29 20:21:54 +0000

    Remove return

--- src/Weather.js
+++ src/Weather.js
@@ -16,17 +16,5 @@ export default function Weather() {
 
 
     return (
-        <div className="html">
-            <h1>Todays Weather</h1>
-            <div className="search">
-                <form className="searchbutton" id="search-form">
-                    <input className="city-search" placeholder="Enter a place..." autocomplete="off" autofocus="on" />
-                    <input className="search-submit" type="submit" value="search" />
-                    <h2 className="date">Date</h2>
-                    <h3>Search for a place</h3>
-                </form>
-            </div>
-            <div className="container"></div>
-        </div>
     )
 }
