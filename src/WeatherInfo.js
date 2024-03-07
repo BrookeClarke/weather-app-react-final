6451e436384e44a7456abf4920f3d7e515de0661
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-03-07 22:29:27 +0000

    Added Icon + Celsius onto same line

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,20 +7,24 @@ export default function WeatherInfo(props) {
                 <FormattedDate date={props.data.date} />
                 <ul>
                     <li>{props.data.city}</li>
-                    <li className="text-capitalize">{props.data.description}</li>
+                    <li className="text-capitalize">{props.data.temperature}</li>
                 </ul>
-                <div className="row">
-                    <div className="col-6">
+            <div className="row">
+                <div className="col-6">
+                    <div className="d-flex">
                         <img src={props.data.iconUrl} alt={props.data.description} />
-                        <h4>{props.data.temperature}</h4>
-                    </div>
-                    <div className="col-6">
-                        <ul>
-                            <li>{props.data.humidity}%</li>
-                            <li>{props.data.wind}km/h</li>
-                        </ul>
+                        <span className="celius">
+                            <h4 className="Temperature">{props.data.temperature}</h4>
+                            °C</span>
                     </div>
                 </div>
+                <div className="col-6">
+                    <ul>
+                        <li>{props.data.humidity}%</li>
+                        <li>{props.data.wind}km/h</li>
+                    </ul>
+                </div>
             </div>
+        </div>
     );
 }
