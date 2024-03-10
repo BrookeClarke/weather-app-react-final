1acc894b5b216e72f6c416b3c985845fee017096
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-03-10 15:11:14 +0000

    Fixing Icon Forecast issue

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,10 +4,11 @@ import WeatherInfo from "./WeatherInfo.js";
 export default function WeatherForecast(props) {
     return (
         <div className="forecast">
+            <WeatherInfo />
             <div className="row">
                 <div className="col">
                     <h4 className="forecast-day">Thurs</h4>
-                    <WeatherInfo code={props.data.weather[0].iconUrl} alt={props.data.description} />
+                    <WeatherInfo code={props.data.iconUrl} alt={props.data.description} />
                     <h4 className="forecast-temperatures">
                         <span className="temperature-maximum">{Math.round(props.data.temperatureMaximum)}°C</span>
                         <span className="temperature-minimum">{Math.round(props.data.temperatureMinimum)}°C</span>
