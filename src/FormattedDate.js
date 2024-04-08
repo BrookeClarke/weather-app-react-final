f2325093a68866a50616b23b3a76f80539445f60
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-04-08 20:24:30 +0100

    .

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -7,15 +7,17 @@ export default function FormattedDate(props) {
     if (hours < 10) {
         hours = `0${hours}`;
     }
+
     let minutes = props.date.getMinutes();
     if (minutes < 10) {
         minutes = `0${minutes}`;
     }
+
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let month = months[props.date.getMonth()];
     let year = props.date.getFullYear();
  
-  
+    
     return (
     <div>
         {day} {month} {hours}:{minutes} {year}
