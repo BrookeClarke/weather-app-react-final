febd5464b904e272c3d4a95a14e9ccdb0f23631f
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-08-14 21:51:31 +0100

    Added new file

--- src/Weather.js
+++ src/Weather.js
@@ -30,7 +30,7 @@ export default function Weather(props) {
     const lat = response.data.coordinates.latitude;
     let roadApiKey = "cd876a10c23602b6fbd5ba8f87584931";
     let roadApi = `https://api.openweathermap.org/data/2.5/onecall?appid=${roadApiKey}&lon=${lon}&lat=${lat}&units=metric`;
-    axios.get(roadApi).then(displayRoadForecast);
+    axios.get(roadApi).then(getRoadTemperature);
   }
 
   function handleResponseDaily(response) {
