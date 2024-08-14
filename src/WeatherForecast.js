7eed92c79c52e8a04e70139335f380461bec67bf
BrookeClarke <129331928+BrookeClarke@users.noreply.github.com>
2024-08-14 21:45:51 +0100

    .

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,47 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios'
+import WeatherForecastDay from './WeatherForecastDay'
 
 export default function WeatherForecast(props) {
-  function maxTemperature() {
-    let temperatureMax = Math.round(props.data.temperature.maximum);
-    
-    if(props.unit === "fahrenheit") {
-      temperatureMax = (temperatureMax * 9) / 5 + 32;
-      return `${temperatureMax}°F`
-    }
-    return `${temperatureMax}°C`;
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false)
+  }, [props.coordinates])
+
+  function displayForecast(response) {
+    setForecast(response.data.daily)
+    setLoaded(true)
   }
 
-  function minTemperature() {
-    let temperatureMin = Math.round(props.data.temperature.minimum);
-
-    if(props.unit === "fahrenheit") {
-      temperatureMin = (temperatureMin * 9) / 5 + 32;
-      return `${temperatureMin}°F`
-    }
-    return `${temperatureMin}°C`;
+  function getForecast() {
+    const lon = props.coordinates.longitude;
+    const lat = props.coordinates.latitude;
+    const apiKey = "0f605ca33b8d413fa995ab3t060267od";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${lat}&lon=${lon}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
   }
 
-  function forecastDay() {
-    let date = new Date(props.data.time * 1000);
-    let day = date.getDay();
-    let days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
-
-    return days[day];
-  }
-
-  return (
-    <div className="forecast">
-      <div className="row">
-        <div className="col">
-          <h4 className="forecast-day">{forecastDay()}</h4>
-          <img src={props.data.condition.icon_url} alt={props.data.condition.description} />
-          <h4 className="forecast-temperatures">
-            <span className="temperature-maximum">{maxTemperature()}</span>
-            <span className="temperature-minimum">{minTemperature()}</span>
-          </h4>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 8) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay unit={props.unit} setUnit={props.setUnit} data={dailyForecast} />
+                </div>
+              );
+            }
+            return null;
+          })}
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    getForecast()
+  }
 }
